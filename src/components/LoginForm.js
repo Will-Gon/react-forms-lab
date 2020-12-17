1fa78a022e115b1419d3e81a764d1c69116e93f2
Will-Gon <will.colocho@gmail.com>
2020-12-16 20:25:20 -0800

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,40 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
   }
 
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const username = this.state.username
+    const password = this.state.password
+    if (username !== '' && password !== '') {
+      this.props.handleLogin(this.state)
+      }
+    }
+  
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={this.handleChange} value={this.state.value} id="username" name="username" type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={this.handleChange} value={this.state.value} id="password" name="password" type="password" />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,22 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = { 
+      value: ''
+    };
   }
-
+  
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+  }
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input value={this.state.value} onChange={this.handleChange} type="text" name="message" id="message" />
+        <p>{this.props.maxChars - this.state.value.length} characters remaining!</p>
       </div>
     );
   }
